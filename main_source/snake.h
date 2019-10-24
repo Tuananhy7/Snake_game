2342b933f1d27906a2a93967343d53cd5e701ee1
Tuananhy7 <tuananh170697@gmail.com>
2019-10-24 12:21:24 -0700

    update forder

---
+++ main_source/snake.h
@@ -0,0 +1,30 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+enum wall_snake_empty_eat
+{
+    EMPTY_BLOCK,
+    WALL_BLOCK,
+    SNAKE_BLOCK,
+    EAT_BLOCK
+};
+
+enum snake_status
+{
+    LIVE_SNAKE,
+    DIE_SNAKE
+};
+
+struct snake
+{
+    int x;
+    int y;
+    struct snake *next;
+};
+
+struct snake *node_snake_create(int x, int y);
+struct snake *add_node_snake( struct snake * head, int x, int y);
+struct snake *appear_snake(struct snake *head, char move_type);
+void delete_snake_tail(struct snake *head);
+
+
+#endif
